6b047b1d164df6856ffc2f9435ff7905433b3450
Sergi Vaqué Galdón <sergivaque@gmail.com>
2023-03-24 23:38:56 +0100

    Level 1 completed

--- js/shop.js
+++ js/shop.js
@@ -1,128 +1,170 @@
 // If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
 var products = [
-   {
-        id: 1,
-        name: 'cooking oil',
-        price: 10.5,
-        type: 'grocery',
-        offer: {
-            number: 3,
-            percent: 20
-        }
+  {
+    id: 1,
+    name: "cooking oil",
+    price: 10.5,
+    type: "grocery",
+    offer: {
+      number: 3,
+      percent: 20,
     },
-    {
-        id: 2,
-        name: 'Pasta',
-        price: 6.25,
-        type: 'grocery'
+  },
+  {
+    id: 2,
+    name: "Pasta",
+    price: 6.25,
+    type: "grocery",
+  },
+  {
+    id: 3,
+    name: "Instant cupcake mixture",
+    price: 5,
+    type: "grocery",
+    offer: {
+      number: 10,
+      percent: 30,
     },
-    {
-        id: 3,
-        name: 'Instant cupcake mixture',
-        price: 5,
-        type: 'grocery',
-        offer: {
-            number: 10,
-            percent: 30
-        }
-    },
-    {
-        id: 4,
-        name: 'All-in-one',
-        price: 260,
-        type: 'beauty'
-    },
-    {
-        id: 5,
-        name: 'Zero Make-up Kit',
-        price: 20.5,
-        type: 'beauty'
-    },
-    {
-        id: 6,
-        name: 'Lip Tints',
-        price: 12.75,
-        type: 'beauty'
-    },
-    {
-        id: 7,
-        name: 'Lawn Dress',
-        price: 15,
-        type: 'clothes'
-    },
-    {
-        id: 8,
-        name: 'Lawn-Chiffon Combo',
-        price: 19.99,
-        type: 'clothes'
-    },
-    {
-        id: 9,
-        name: 'Toddler Frock',
-        price: 9.99,
-        type: 'clothes'
-    }
-]
+  },
+  {
+    id: 4,
+    name: "All-in-one",
+    price: 260,
+    type: "beauty",
+  },
+  {
+    id: 5,
+    name: "Zero Make-up Kit",
+    price: 20.5,
+    type: "beauty",
+  },
+  {
+    id: 6,
+    name: "Lip Tints",
+    price: 12.75,
+    type: "beauty",
+  },
+  {
+    id: 7,
+    name: "Lawn Dress",
+    price: 15,
+    type: "clothes",
+  },
+  {
+    id: 8,
+    name: "Lawn-Chiffon Combo",
+    price: 19.99,
+    type: "clothes",
+  },
+  {
+    id: 9,
+    name: "Toddler Frock",
+    price: 9.99,
+    type: "clothes",
+  },
+];
 // Array with products (objects) added directly with push(). Products in this array are repeated.
 var cartList = [];
 
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
 var cart = [];
 
-var total = 0;
+var totalPrice = 0;
+var totalItems = document.querySelector('#count_product');
+var productsTable = document.querySelector('#cart_list'); 
 
 // Exercise 1
 function buy(id) {
-    let productSelected = {};
-    // 1. Loop for to the array products to get the item to add to cart
-    
-    // products.forEach(product => product.id === id ? productSelected = product : '');        
-    // productSelected = products.filter(product => product.id === id)[0];
-    productSelected = products.find(product => product.id === id);
- 
-    // 2. Add found product to the cartList array
-    //cartList.unshift(productSelected); // Addig at the beginning
-    cartList.push(productSelected);
+  let productSelected = {};
+  // 1. Loop for to the array products to get the item to add to cart
+  // products.forEach(product => product.id === id ? productSelected = product : '');
+  // productSelected = products.filter(product => product.id === id)[0];
+  productSelected = products.find((product) => product.id === id);
 
+  // 2. Add found product to the cartList array
+  // cartList.unshift(productSelected); // Adding item add at beginnig of the array
+  cartList.push(productSelected);
+  totalItems.innerHTML = cartList.length;
+  
+  calculateTotal();
 }
 
 // Exercise 2
 function cleanCart() {
-    cartList = [];
+  cartList = [];
+  cart = [];
+  totalPrice = 0;
+  totalItems.innerHTML = 0;    
+  //productsTable.remove(); 
+  document.querySelector('#total_price').innerHTML = 0;
+  
+  while(productsTable.hasChildNodes()){
+    productsTable.removeChild(productsTable.firstChild);
+  }
 }
 
 // Exercise 3
 function calculateTotal() {
-    // Calculate total price of the cart using the "cartList" array
-    return cartList.reduce((accumulator, product) => accumulator + product.price, 0);
+  totalPrice = cartList.reduce(
+    (accumulator, product) => accumulator + product.price,
+    0
+  );
 }
 
 // Exercise 4
-function generateCart() {
-    // Using the "cartlist" array that contains all the items in the shopping cart, 
-    // generate the "cart" array that does not contain repeated items, 
-    // instead each item of this array "cart" shows the quantity of product.
-    const cart = [];
-    cartList.forEach(product => {
-        if (cart.find(item => item.id === product.id)) {
-            cart
-        } else {
-            cart.push(product);
-        }
+function generateCart(cartList) {
+  let cartToCompute = [];
+  let groceryItems = 0;
 
-    })
+  cartList.forEach((product) => {
+    console.log(`cartList: ${product.name}`);
+    if (cartToCompute.find((item) => item.id === product.id)) {
+      product.quantity++;
+    } else {
+      product.quantity = 1;
+      product.subtotalWithDiscount = 0;      
+      cartToCompute.push(product);
+    }
+    product.subtotal = product.quantity * product.price;
+    if (product.type === "grocery") groceryItems++;
+  });
 
-    return cart;
+  applyPromotionsCart(cartToCompute, groceryItems > 9);
 }
 
 // Exercise 5
-function applyPromotionsCart() {
-    // Apply promotions to each item in the array "cart"
+function applyPromotionsCart(cartToCompute, isGroceryDiscount) {
+  cartToCompute.forEach((product) => {
+    console.log(`cartToCompute: ${product.name}`);
+    if (product.type === "grocery" && isGroceryDiscount) {
+        product.subtotalWithDiscount = product.subtotal - product.subtotal * (2 / 3);
+        cart.push(product);
+        return;
+    }
+    
+    if (product.offer !== undefined && product.quantity >= product.offer.number) {
+        product.subtotalWithDiscount = product.subtotal - product.subtotal * (product.offer.percent / 100);
+        cart.push(product);
+        return;
+    }
+    product.subtotalWithDiscount = product.subtotal;
+    cart.push(product);
+  });
+
 }
 
 // Exercise 6
 function printCart() {
-    // Fill the shopping cart modal manipulating the shopping cart dom
+  document.querySelector('#total_price').innerHTML = totalPrice;
+  cart.forEach(product => {
+    console.log(`cart: ${product.name}`);
+    var newProductRow = productsTable.insertRow(-1);
+    newProductRow.insertCell(0).innerHTML = product.name;
+    newProductRow.insertCell(1).innerHTML = '$' + product.price;
+    newProductRow.insertCell(2).innerHTML = product.quantity;
+    newProductRow.insertCell(3).innerHTML = '$' + product.subtotalWithDiscount;  
+  });
+  
 }
 
 
@@ -130,18 +172,19 @@ function printCart() {
 
 // Exercise 7
 function addToCart(id) {
-    // Refactor previous code in order to simplify it 
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cart array or update its quantity in case it has been added previously.
+  // Refactor previous code in order to simplify it
+  // 1. Loop for to the array products to get the item to add to cart
+  // 2. Add found product to the cart array or update its quantity in case it has been added previously.
 }
 
 // Exercise 8
 function removeFromCart(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+  // 1. Loop for to the array products to get the item to remove to cart
+  // 2. Remove found product to the cartList array
 }
 
-function open_modal(){
-	console.log("Open Modal");
-	printCart();
+function open_modal() {
+  console.log('open_modal');
+  generateCart(cartList);
+  printCart();
 }
